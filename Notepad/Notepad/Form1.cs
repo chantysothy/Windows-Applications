5f13f071fc023c16fa4f587daccb3d203d88f395
Sothy Chanty <sothy.chanty@okoone.com>
2016-08-23 10:41:52 +0700

    Remove ZeroSpace

--- Notepad/Notepad/Form1.cs
+++ Notepad/Notepad/Form1.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 using System.Text;
 using System.IO;
@@ -14,7 +14,7 @@ namespace Notepad
 {
     public partial class Form1 : Form
     {
-        RichTextBox rtb;
+        TextBox rtb;
         public Form1()
         {
             InitializeComponent();
@@ -24,20 +24,23 @@ namespace Notepad
         {
 
         }
-        private RichTextBox GetRichTextBox()
+        private TextBox GetRichTextBox()
         {
             rtb = null; //making it initially null
             TabPage tp = tabControl1.SelectedTab; // saves the tab selection status in a tabpage type variable
             if(tp!=null)
             {
-                rtb = tp.Controls[0] as RichTextBox; //sets the selected rich text box index as primary
+                rtb = tp.Controls[0] as TextBox; //sets the selected rich text box index as primary
             }
             return rtb;
         }
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
             TabPage tp = new TabPage("New Document"); //creates a new tab page
-            rtb = new RichTextBox(); //creates a new richtext box object
+            rtb = new TextBox(); //creates a new richtext box object
+            rtb.Multiline = true;
+            rtb.Font = new Font("Khmer OS System", 12);
+            rtb.MaxLength = 0;
             rtb.Dock = DockStyle.Fill; //docks rich text box 
             tp.Controls.Add(rtb); // adds rich text box to the tab page
             tabControl1.TabPages.Add(tp); //adds the tab pages to tab control 
@@ -68,7 +71,10 @@ namespace Notepad
                     string filename = openFileDialog1.FileName;//copies the path of the file into a variable
                     string readfiletext = File.ReadAllText(filename);//reads all the text from the opened file
                     TabPage tp = new TabPage("New Document"); //creates a new tab page
-                    rtb = new RichTextBox(); //creates a new richtext box object
+                    rtb = new TextBox(); //creates a new richtext box object
+                    rtb.Font= new Font("Khmer OS System",12);
+                    rtb.Multiline = true;
+                    rtb.MaxLength = 0;
                     rtb.Dock = DockStyle.Fill; //docks rich text box 
                     tp.Controls.Add(rtb); // adds rich text box to the tab page
                     tabControl1.TabPages.Add(tp); //adds the tab pages to tab control
@@ -84,8 +90,56 @@ namespace Notepad
             savefile.Filter = "*.txt(textfile)|*.txt";
             if(savefile.ShowDialog()==DialogResult.OK)
             {
-              rtb.SaveFile(savefile.FileName, RichTextBoxStreamType.PlainText);
+              //rtb.SaveFile(savefile.FileName, RichTextBoxStreamType.PlainText);
             }
+        }
+
+        private void newToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            TabPage tp = new TabPage("New Document"); //creates a new tab page
+            rtb = new TextBox(); //creates a new richtext box object
+            rtb.Multiline = true;
+            rtb.Font = new Font("Khmer OS System", 12);
+            rtb.MaxLength = 0;
+            rtb.Dock = DockStyle.Fill; //docks rich text box 
+            tp.Controls.Add(rtb); // adds rich text box to the tab page
+            tabControl1.TabPages.Add(tp); //adds the tab pages to tab control 
+        }
+
+        private void cutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            GetRichTextBox().Cut();
+        }
+
+        private void copyToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            GetRichTextBox().Copy();
+        }
+
+        private void pasteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            GetRichTextBox().Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetRichTextBox().SelectAll();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetRichTextBox().Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            var tmp = GetRichTextBox();
+            tmp.Text = tmp.Text.Replace("​", string.Empty);
         }
     }
 }
